6e42acb4e9c90c4bf83573418168373457e6e2b9
vsavko <35723434+vsavko@users.noreply.github.com>
2019-11-12 11:29:16 +0300

    Update 001_TwoSum.java

--- 001_TwoSum.java
+++ 001_TwoSum.java
@@ -1,4 +1,5 @@
 class Solution {
+    //brute force with O(n^2) time complexity, and O(1) memory
     public int[] twoSum(int[] nums, int target) {
         int [] array = new int[2];
         for (int i =0; i < nums.length; i++){
@@ -11,5 +12,17 @@ class Solution {
             }
         }
         return null;
+    }
+    
+    //using a hash map with O(n) time complexity and O(n) memory
+    public static int[] twoSum2(int[] nums, int target) { //how to pass array starting from certain index?
+    	Map<Integer,Integer> hashMap = new HashMap<>();
+    	for(int i = 0; i < nums.length; i++) {
+    		if(hashMap.containsKey(target - nums[i])) {
+    			return new int[] {i, hashMap.get(target - nums[i])};
+    		}
+    		hashMap.put(nums[i], i);
+    	}
+    	return null;
     }
 }
