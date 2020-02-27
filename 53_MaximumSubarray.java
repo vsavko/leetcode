d5953418459ca8adf36ebde21ec6bb6eb2050d8c
vsavko <35723434+vsavko@users.noreply.github.com>
2020-02-27 18:19:15 +0300

    Update 53_MaximumSubarray.java

--- 53_MaximumSubarray.java
+++ 53_MaximumSubarray.java
@@ -10,6 +10,7 @@ Follow up:
 If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.*/
 
 public class MaximumSubarray {
+	
 	public static int maxSubArray(int[] nums) {
 		if (nums == null) return 0;
 		
@@ -30,12 +31,52 @@ public class MaximumSubarray {
 		return max;
 	}
 	
+	public static int maxSubArray2(int[] nums) {
+		if (nums == null) return 0;
+	    //divide and conquer
+		int max = findMaxDNC(nums, 0, nums.length - 1)[2];
+		return max;
+	}
+	
+	public static int[] findMaxDNC(int [] nums, int start, int end) {
+		int [] returnArr = new int[4];
+	
+		if(end == start) {
+			returnArr[0] = nums[start];
+			returnArr[1] = nums[start];
+			returnArr[2] = nums[start];
+			returnArr[3] = nums[start];
+			return returnArr;
+		}
+		
+		int arrLengthLeft = (end - start)/2;
+		
+		int [] left = findMaxDNC(nums, start, start + arrLengthLeft );
+		int [] right = findMaxDNC(nums, start + arrLengthLeft+1, end );
+		
+		//int[0] - left max 
+		//int[1] - right max
+		//int[2] - array max
+		//int[3] - total sum
+		returnArr[0] = Math.max(left[0], left[3] + right[0]);
+		returnArr[1] = Math.max(right[1], right[3] + left[1]);
+		returnArr[2] = Math.max(left[1]+ right[0], Math.max(left[2],right[2]));
+		returnArr[3] = left[3] + right[3];
+		
+		return returnArr;
+	}
+	
+	
 	public static void main(String[] args) {
 		int [] arr = {-2,1,-3,4,-1,2,1,-5,4};
 		int [] arr2 = {-5,7,-5,-23,1};
-		int [] arr3 = {-1,-2,-4};
+		int [] arr3 = {-1,-2,-4,1};
 		System.out.println(maxSubArray(arr));
 		System.out.println(maxSubArray(arr2));
 		System.out.println(maxSubArray(arr3));
+		
+		System.out.println(maxSubArray2(arr));
+		System.out.println(maxSubArray2(arr2));
+		System.out.println(maxSubArray2(arr3));
 	}
 }
