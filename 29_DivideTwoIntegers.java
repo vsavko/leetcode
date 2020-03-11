68e283d3edddf4febf8711e53445d95d3ce43918
vsavko <35723434+vsavko@users.noreply.github.com>
2020-03-11 23:19:37 +0300

    Update 29_DivideTwoIntegers.java

--- 29_DivideTwoIntegers.java
+++ 29_DivideTwoIntegers.java
@@ -1,4 +1,4 @@
-package test;
+package leetcode;
 
 public class DivideTwoIntegers {
     public static int divide(int dividend, int divisor) {
@@ -16,54 +16,61 @@ public class DivideTwoIntegers {
     		++divisorBits;
     		tmp = tmp >> 1;
     	}
-
-    	//System.out.println(Integer.toBinaryString(dividend));
-    	//System.out.println(dividendBits);
-    	//System.out.println(divisorBits);
     	
     	if (divisor > dividend ) return 0;
     	int bitShift = dividendBits - divisorBits, tmpDividendbits = divisorBits;
     	int tmpDividend = dividend >> (dividendBits - divisorBits);
     	int ans = 0;
     	
-        while(dividend > divisor) {
-        	System.out.println("DIV " + Integer.toBinaryString(dividend));
+        while(dividend >= divisor ) {
         	
-        	System.out.println("bitShift " + bitShift);
-
         	while(tmpDividend < divisor) {
-        		System.out.println("test");
+        		
+        		//System.out.println("test " + dividend);
         		++tmpDividendbits;
         		tmpDividend = dividend >> (bitShift - 1) ;
         		ans = ans << 1;
         		++bitShift;
         	}
-        	System.out.println(Integer.toBinaryString(tmpDividend));
         	
         	if(tmpDividend >= divisor) {
+        		ans = ans << 1;
         		ans |= 1;
         		tmpDividend -= divisor;
-        		tmpDividend = tmpDividend << (dividendBits - tmpDividendbits);
-        		int mask = 1 << (dividendBits - tmpDividendbits - 1);
-        		tmpDividend += dividend & mask;
-        		dividend = tmpDividend;
+        		int mask = ~(~0 << (dividendBits - tmpDividendbits ));
+
+        		dividend =  (tmpDividend << (dividendBits - tmpDividendbits)) + (dividend & mask);
         		
-        		
+           		while( dividend == 0 && dividendBits > tmpDividendbits) { //check for remaining zeros
+           			System.out.println(dividendBits);
+	    			ans = ans << 1;
+	    			--dividendBits;
+           		}
+
+        		dividendBits = 0;
         		tmp = dividend;
+        		
             	while(tmp > 0) {
             		++dividendBits;
             		tmp = tmp >> 1;
             	}
             	
-        		bitShift = dividendBits - 1;
+            	tmp = tmpDividend;	
+            	tmpDividendbits = 0;
+            	while(tmp > 0) {
+            		++tmpDividendbits;
+            		tmp = tmp >> 1;
+            	}
+            	bitShift = tmpDividendbits;
+            	++tmpDividendbits;
         	}
         	
-        	System.out.println("dividend " + Integer.toBinaryString(dividend) + " ans " + ans);
+        	System.out.println("dividend " + Integer.toBinaryString(dividend) + " ans " + ans + " tmpDividend " + tmpDividend);
         }
         return ans;
     }
     
     public static void main(String[] args) {
-		System.out.println(divide(74,10));
+		System.out.println(divide(25,10));
 	}
 }
