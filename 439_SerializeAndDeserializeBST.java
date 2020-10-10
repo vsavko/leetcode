154c9f2a2a0f282b27207f603af37881069adc91
vsavko <35723434+vsavko@users.noreply.github.com>
2020-10-10 18:12:39 +0300

    Create 439_SerializeAndDeserializeBST.java

---
+++ 439_SerializeAndDeserializeBST.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+	
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+    	if (root == null)
+    		return "";
+        StringBuilder codedTree = new StringBuilder();
+        codedTree.append(root.val);
+        codedTree.append(',');
+        if (root.left != null)
+        	codedTree.append(serialize(root.left));
+        if (root.right != null)		
+        	codedTree.append(serialize(root.right));
+        return codedTree.toString();
+    }
+
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+    	if (data == "") return null;
+    	String[] items = data.split(",");
+    	//System.out.println(Arrays.toString(items) + items.length);
+    	if (items.length < 1) return null;
+    	Stack<TreeNode> stack = new Stack<>();
+    	
+    	TreeNode ansNode = new TreeNode(Integer.parseInt(items[0]));
+    	TreeNode currentNode = ansNode;
+    	
+    	for (int i = 1; i < items.length; i++) {
+    		String item = items[i];
+    		int intItem = Integer.parseInt(item);
+    		TreeNode node = new TreeNode(intItem);
+ 
+			if (currentNode.val > intItem) {
+				currentNode.left = node;
+				stack.push(currentNode);
+			}
+			else {
+				while(!stack.isEmpty() && stack.peek().val < node.val)
+					currentNode = stack.pop();
+				currentNode.right = node;
+			}   
+			currentNode = node;
+    	}
+    	//System.out.println(Arrays.toString(items));
+    	//System.out.println(stack.firstElement().val);
+    	return ansNode;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// String tree = ser.serialize(root);
+// TreeNode ans = deser.deserialize(tree);
+// return ans;
